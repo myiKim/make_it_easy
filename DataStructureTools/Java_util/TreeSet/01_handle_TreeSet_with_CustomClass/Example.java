9b2ffafd3baee70d0d21d5775b5a0c8e061f5b03
myiSuwon <myungkim87titan@gmail.com>
2023-03-23 08:52:53 +0900

    treeset pollfirst applied

--- DataStructureTools/Java_util/TreeSet/01_handle_TreeSet_with_CustomClass/Example.java
+++ DataStructureTools/Java_util/TreeSet/01_handle_TreeSet_with_CustomClass/Example.java
@@ -1,39 +1,43 @@
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.TreeSet;
+import java.util.Comparator;
+
+class Player{
+
+	int pId, birthyear, height, weight;
+	String position, name, surname;
+	String[] positions = new String[]{"GK", "DF", "MF", "FW"};
+
+	Player(int pId, int birthyear, int height, int weight, int position, String name, String surname){
+		this.pId = pId;
+		this.birthyear = birthyear;
+		this.height = height;
+		this.weight = weight;
+		this.position = positions[position];
+		this.name = name;
+		this.surname = surname;
+	}
+
+}
+
+
 
 class Example{
 
 	public static Scanner txtreader;
-	public static String[] positions = new String[]{"GK", "DF", "MF", "FW"};
+	
+	public static Comparator<Player> compar= new Comparator<Player>(){
+    	@Override
+        public int compare(Player a , Player b){
+    	    if(a.birthyear ==b.birthyear){
 
-
-	public static class Player{
-
-		int pId, birthyear, height, weight;
-		String position, name, surname;
-		Player(int pId, int birthyear, int height, int weight, int position, String name, String surname){
-			this.pId = pId;
-			this.birthyear = birthyear;
-			this.height = height;
-			this.weight = weight;
-			this.position = positions[position];
-			this.name = name;
-			this.surname = surname;
-		}
-
-	}
-
-	// public class PlayerComparator implements Comparator<Player>{
-	// 	@Override
-	// 	public int compare(Player p1, Player p2) {
-	// 		if(p1.birthyear == p2.birthyear) return p1.pId - p2.pId;
-	// 		return p1.birthyear - p2.birthyear;
-	// 	}
-		
-	// }
-	// PlayerComparator compar = new PlayerComparator();
-	// TreeSet<Player> playerTree = new TreeSet<Player>(compar);
+    			return a.pId-b.pId;
+        	}
+        	return a.birthyear - b.birthyear;
+        }        
+    };
+	public static TreeSet<Player> playerTree = new TreeSet<Player>(compar);
 
 	public static void main(String[] args) throws Exception{
 
@@ -54,9 +58,20 @@ class Example{
 			Player pp = new Player(playerId, playerBirthYear, playerHeight, playerWeight, playerPosition, playerName, playerSurname);
 			// System.out.println(pp.height + "is player's height!");
 			// System.out.println(pp.name + "<-- his name!");
-			// playerTree.add(pp);
+			playerTree.add(pp);
 
 		}
+		
+		while(true){
+			Player popped = playerTree.pollFirst();
+
+			if(popped == null) break;
+			System.out.println("now popped a player!");
+			System.out.println("Player ID:" + popped.pId);
+			System.out.println("Player Name:" + popped.name);
+
+		}
+
 		txtreader.close();
 	}
 
