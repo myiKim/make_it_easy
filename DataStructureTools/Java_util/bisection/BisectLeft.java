e2085f80e5613d2937abddbca85c50706c5b0771
MyiKim <102040823+myiKim@users.noreply.github.com>
2023-09-01 22:59:16 +0900

    Created BisectLeft.java
    for reference

---
+++ DataStructureTools/Java_util/bisection/BisectLeft.java
@@ -0,0 +1,19 @@
+// BisectLeft.java
+
+public class BisectLeft {
+
+    public static int bisectLeft(int[] arr, int x) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+}
